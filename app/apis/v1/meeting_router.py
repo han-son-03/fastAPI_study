197754182a032281e0801738f712bdcaafc68f0b
han-son-03 <rkdgksths11@naver.com>
2025-08-22 10:19:15 +0900

    implement create meeting edgedb

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,19 +1,17 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.services.meeting_service_edgedb import service_create_meeting_edgedb
 
-edgedb_router = APIRouter(prefix="/v1/edgedb/meeting", tags=["meeting"], redirect_slashes=False)
-mysql_router = APIRouter(prefix="/v1/mysql/meeting", tags=["meeting"], redirect_slashes=False)
+edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
+mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
 
 
-@edgedb_router.post(
-    "",
-    description="meeting 을 생성합니다.",
-)
+@edgedb_router.post("", description="meeting 을 생성합니다.")
 async def api_create_meeting_edgedb() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+    return CreateMeetingResponse(url_code=(await service_create_meeting_edgedb()).url_code)
 
 
 @mysql_router.post("", description="meeting 을 생성합니다.")
-async def api_create_meeting_myql() -> CreateMeetingResponse:
+async def api_create_meeting_mysql() -> CreateMeetingResponse:
     return CreateMeetingResponse(url_code="abc")
